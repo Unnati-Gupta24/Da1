fd95a9ea90c689dae5d35aa5b8965490c88551aa
Unnati gupta <guptaunnati031@gmail.com>
2024-10-21 16:04:43 +0530

    Update common.go

--- Da/common.go
+++ Da/common.go
@@ -1,9 +1,11 @@
 package da
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"os/exec"
+	"gopkg.in/zeromq/goczmq.v4"
 )
 
 // Global variables for script paths
@@ -16,8 +18,12 @@ var (
 type ChannelReader interface {
 	subscribe(endpoint string, typ string) bool
 	clear() bool
-	listen(processor RawBlockProcessor)
 	validate() (bool, [][]byte)
+}
+
+// Define RawBlockProcessor interface here if not imported from another file.
+type RawBlockProcessor interface {
+	Process(msg [][]byte) error 
 }
 
 // CallScriptWithData executes a bash script with the given data and returns the output.
@@ -25,7 +31,10 @@ func CallScriptWithData(scriptPath string, data string, btcCliPath string) ([]by
 	cmd := exec.Command(scriptPath+"/op_return_transaction.sh", data)
 	cmd.Env = append(os.Environ(), "BTC_CLI_PATH="+btcCliPath)
 	out, err := cmd.Output()
-	return out, err
+	if err != nil {
+		return nil, fmt.Errorf("failed to execute script %s: %w", scriptPath, err)
+	}
+	return out, nil
 }
 
 // ValidateMessage checks if the received message is valid.
@@ -35,4 +44,34 @@ func ValidateMessage(msg [][]byte) (bool, [][]byte) {
 		return false, nil
 	}
 	return true, msg
+}
+
+// StartListening starts listening for messages on the given channeler and processes them.
+func StartListening(channeler *goczmq.Channeler, processor RawBlockProcessor, writeInterval int) {
+	counter := 0
+
+	for {
+		msg, ok := <-channeler.RecvChan
+		if !ok {
+			log.Println("Failed to receive message")
+			continue
+		}
+		if (counter % writeInterval) != 0 {
+			continue
+		}
+
+		valid, msg := ValidateMessage(msg)
+		if !valid {
+			continue
+		}
+
+		log.Println("Processing message")
+		err := processor.Process(msg)
+		if err != nil {
+			log.Println("Error processing message:", err)
+			continue
+		}
+
+		counter++
+	}
 }
